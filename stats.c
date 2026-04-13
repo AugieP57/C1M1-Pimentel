41ef4e146dc75a73e2ecb355cc1fb70412fc1856
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2026-04-13 11:50:40 -0500

    Add files via upload
    second commit

---
+++ stats.c
@@ -0,0 +1,54 @@
+/******************************************************************************
+* File: stats.c
+* Author: Augustine Pimentel
+* Description:
+* Performs statistical analysis on a dataset.
+******************************************************************************/
+
+#include "stats.h"
+#include <stdio.h>
+
+
+/* Test dataset */
+unsigned char test[SIZE] = {
+201, 203, 199, 190, 210, 180, 175, 220,
+205, 198, 200, 202, 204, 206, 207, 208,
+210, 212, 214, 215, 217, 219, 221, 223,
+225, 227, 229, 231, 233, 235, 237, 239,
+241, 243, 245, 247, 249, 250, 252, 255
+};
+
+/* Main function */
+int main(void)
+{
+    
+    return 0;
+}
+
+/* Print statistics */
+void print_statistics(unsigned char *array, unsigned int length)
+{}
+
+/* Print array elements */
+void print_array(unsigned char *array, unsigned int length)
+{}
+
+/* Find mean value */
+unsigned char find_mean(unsigned char *array, unsigned int length)
+{}
+
+/* Find maximum value */
+unsigned char find_maximum(unsigned char *array, unsigned int length)
+{}
+
+/* Find minimum value */
+unsigned char find_minimum(unsigned char *array, unsigned int length)
+{}
+
+/* Find median */
+unsigned char find_median(unsigned char *array, unsigned int length)
+{}
+
+/* Sort array (largest → smallest) using bubble sort */
+void sort_array(unsigned char *array, unsigned int length)
+{}
